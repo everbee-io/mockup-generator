bd1f6c8d9063e689bc75c5207fb5d49bb295c52a
pawan-everbee <pawan@everbee.io>
2023-12-26 12:39:09 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,89 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from "react";
 import './App.css';
-import GenerateMockup from './pixi';
+import * as PIXI from 'pixi.js';
+import { Loader } from "@pixi/loaders";
+import setupPixi, { convertB64toBlob, getAlphaUrl } from "./pixi/pixi-func";
 
 function App() {
+  const [mockupLoader, setLoading] = useState(false);
+  const [productData, setProductData] = useState<any>();
+  const searchParams = new URLSearchParams(window.location.search);
+  const orderId = searchParams.get('orderId');
+  const catalogSKUId = searchParams.get('catalogSKUId');
+  const productId = searchParams.get('productId');
+  const apiBaseURL = process.env.REACT_APP_BACKEND_URL;
+
+  const getProductData = useCallback(async () => {
+    if (!productData) {
+      console.log('hello');
+      const response = await fetch(`${apiBaseURL}/orders/data-for-preview-mockup?catalogSKUId=${catalogSKUId}&productId=${productId}`);
+      const data = await response.json();
+      console.log('data.result', data.result);
+      setProductData(data.result);
+    }
+  }, [apiBaseURL, catalogSKUId, productId, productData]);
+
+  const processAngles = async (data: any, angles: any) => {
+    const images: any = {};
+    await Promise.all(
+      angles.map(async (angle: any) => {
+        const app = new PIXI.Application({
+          width: data.originalWidth,
+          height: data.originalHeight,
+          backgroundColor: 0xffffff,
+        });
+        const loader = new Loader();
+        setLoading(true);
+        const image: any = await new Promise(async (resolve) => {
+          await setupPixi(
+            data.colorName.toLowerCase().includes('heather') ? data.productMainImages[angle].heather : data.productMainImages[angle].normal,
+            getAlphaUrl(angle, data.colorName, data),
+            angle === 'front' ? data.frontDesignFile : data.backDesign,
+            data.productAlphaImages[angle].normal,
+            data.boundaryWidth[angle],
+            data.boundaryHeight[angle],
+            data.boundaryX[angle],
+            data.boundaryY[angle],
+            data.colorName + angle,
+            data.curvesDifficulty,
+            `${data.colorCode}`,
+            angle,
+            resolve,
+            app,
+            data.originalWidth,
+            data.originalHeight,
+            loader,
+            'full',
+            'full',
+          )
+        });
+        const imageBlob = convertB64toBlob(image.src);
+        const formData = new FormData();
+        formData.set('images', imageBlob, `${angle}previewFile-${productId}.png`);
+        const request = await fetch(`${apiBaseURL}/users/temp-gallery`, {
+          body: formData,
+          method: 'POST'
+        });
+        const { src } = await request.json();
+        images[angle] = src;
+        setLoading(false);
+      })
+    );
+    return images;
+  };
+
+  useEffect(() => {
+    console.log('Before getProductData');
+    getProductData();
+    console.log('After getProductData');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getProductData]);
+  
+
   return (
-    <GenerateMockup />
+    <>
+      {mockupLoader && <p>Loading.....</p>}
+    </>
   );
 }
 
